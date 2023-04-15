4c86843022ee11a70b30c1d4a25a2efab60a180e
HamidFreig <h.morafreig@uandresbello.edu>
2023-04-14 23:19:41 -0400

    avance

--- src/components/LoginView/LoginView.jsx
+++ src/components/LoginView/LoginView.jsx
@@ -1,56 +1,77 @@
 import "./LoginView.css";
 import { getFirestore, getDocs, collection } from "firebase/firestore";
-import { useState, useEffect, useImperativeHandle } from "react";
+import { useState, useEffect } from "react";
 
 export const LoginView = () => {
-    const [usersList, setUserList] = useState([]);
+  const [usersList, setUserList] = useState([]);
+  const [datos, setDatos] = useState({
+    rut: "",
+    passw: "",
+  });
 
-    const getUsuarios = () => {
-        const db = getFirestore();
-        const querySnapshot = collection(db, "Usuarios");
-        getDocs(querySnapshot)
-            .then((response) => {
-                const listUsers = response.docs.map((doc) => {
-                    return { id: doc.id, ...doc.data() };
-                });
-                setUserList(listUsers);
-            })
-            .catch((error) => console.log(error));
-    };
+  const HandleInputChange = (event) => {
+    //GUARDAR LOS DATOS TECLEADOS DE LOS INPUTS EN EL STATE
+    setDatos({
+      ...datos,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    useEffect(() => {
-        setTimeout(() => {
-            getUsuarios();
-            console.log("SDAf");
-            console.log(usersList);
-        }, "2000");
-    }, []);
+  const getUsuarios = () => {
+    //ACCEDER A LOS USER DE LA DB DE FIRESTORE
+    const db = getFirestore();
+    const querySnapshot = collection(db, "Usuarios");
+    getDocs(querySnapshot)
+      .then((response) => {
+        const listUsers = response.docs.map((doc) => {
+          return { id: doc.id, ...doc.data() };
+        });
+        setUserList(listUsers);
+      })
+      .catch((error) => console.log(error));
+  };
 
-    return (
-        <div
-            style={{
-                backgroundImage: `url("https://images7.alphacoders.com/374/374296.jpg")`,
-            }}
-        >
-            <div className="login-container">
-                <form>
-                    <h1>Iniciar sesión</h1>
-                    <div className="form-group">
-                        <label name="email">Rut</label>
-                        <input id="email" name="email" required />
-                    </div>
-                    <div className="form-group">
-                        <label name="password">Contraseña</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            required
-                        />
-                    </div>
-                    <button type="submit">Ingresar</button>
-                </form>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    setTimeout(() => {
+      getUsuarios(); //HAGO UN TIMER PARA ALMACENAR LOS DATOS EN EL ARRAY LOCAL
+    }, "2000");
+  }, []);
+
+  const authUser = (event) => {
+    //AUTNETIFICACIÓN DE LOS USUARIOS
+    event.preventDefault();
+    console.log(datos.passw);
+    console.log(datos.rut);
+  };
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url("https://images7.alphacoders.com/374/374296.jpg")`,
+      }}
+    >
+      <div className="login-container">
+        <form>
+          <h1>Iniciar sesión</h1>
+          <div className="form-group">
+            <label name="rut">Rut</label>
+            <input id="rut" name="rut" onChange={HandleInputChange} required />
+          </div>
+          <div className="form-group">
+            <label name="password">Password</label>
+            <input
+              type="password"
+              id="passw"
+              name="passw"
+              onChange={HandleInputChange}
+              required
+            />
+          </div>
+          <button type="submit" onClick={authUser}>
+            Ingresar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
