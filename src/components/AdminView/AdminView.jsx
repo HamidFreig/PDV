4b0d2c90a76107dde7ebb51dc7a106b44ae21cc5
HamidFreig <h.morafreig@uandresbello.edu>
2023-05-09 21:34:47 -0400

    preliminar

--- src/components/AdminView/AdminView.jsx
+++ src/components/AdminView/AdminView.jsx
@@ -4,71 +4,72 @@ import { useContext } from "react";
 import "./AdminView.css";
 
 export const AdminView = () => {
-  const navigate = useNavigate();
-  const { getUsuarios, getProductos } = useContext(BDContext);
+    const navigate = useNavigate();
+    const { getUsuarios, getProductos } = useContext(BDContext);
 
-  const ButtonOut = () => {
-    navigate("/");
-  };
+    const ButtonOut = () => {
+        navigate("/");
+    };
 
-  return (
-    <div
-      style={{
-        backgroundImage: `url("https://img.freepik.com/vector-premium/patron-isometrico-costuras-cubos-linea-delgada_659980-2.jpg")`,
-      }}
-    >
-      <button className="Button-Out" onClick={ButtonOut}>
-        SALIR
-      </button>
-      <label style={{ backgroundColor: "white" }}>
-        PANEL DEL ADMINISTRADOR
-      </label>
-      <div className="Container">
-        <div className="Grid-Buttons">
-          <Link to={"/createUser"}>
-            <button
-              className="Button"
-              onClick={() => {
-                getUsuarios();
-              }}
-            >
-              CREAR VENDEDOR
+    return (
+        <div
+            style={{
+                backgroundImage: `url("https://img.freepik.com/vector-premium/patron-isometrico-costuras-cubos-linea-delgada_659980-2.jpg")`,
+                height: "auto",
+            }}
+        >
+            <button className="Button-Out" onClick={ButtonOut}>
+                SALIR
             </button>
-          </Link>
-          <Link to={"/deleteUser"}>
-            <button
-              className="Button"
-              onClick={() => {
-                getUsuarios();
-              }}
-            >
-              ELIMINAR VENDEDOR
-            </button>
-          </Link>
-          <Link to={"/stock"}>
-            <button
-              className="Button"
-              onClick={() => {
-                getProductos();
-              }}
-            >
-              STOCK
-            </button>
-          </Link>
-          <Link to={"/reception"}>
-            <button
-              className="Button"
-              onClick={() => {
-                getProductos();
-              }}
-            >
-              RECEPCIÓN DE MERCADERIA
-            </button>
-          </Link>
+            <label style={{ backgroundColor: "white" }}>
+                PANEL DEL ADMINISTRADOR
+            </label>
+            <div className="Container">
+                <div className="Grid-Buttons">
+                    <Link to={"/createUser"}>
+                        <button
+                            className="Button"
+                            onClick={() => {
+                                getUsuarios();
+                            }}
+                        >
+                            CREAR VENDEDOR
+                        </button>
+                    </Link>
+                    <Link to={"/deleteUser"}>
+                        <button
+                            className="Button"
+                            onClick={() => {
+                                getUsuarios();
+                            }}
+                        >
+                            ELIMINAR VENDEDOR
+                        </button>
+                    </Link>
+                    <Link to={"/stock"}>
+                        <button
+                            className="Button"
+                            onClick={() => {
+                                getProductos();
+                            }}
+                        >
+                            STOCK
+                        </button>
+                    </Link>
+                    <Link to={"/reception"}>
+                        <button
+                            className="Button"
+                            onClick={() => {
+                                getProductos();
+                            }}
+                        >
+                            RECEPCIÓN DE MERCADERIA
+                        </button>
+                    </Link>
 
-          <button className="Button">LIBRO DE VENTAS</button>
+                    <button className="Button">LIBRO DE VENTAS</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
--- src/context/AllContext.js
+++ src/context/AllContext.js
@@ -1,210 +1,187 @@
 import { BDContext } from "./BDContext";
 import {
-  getFirestore,
-  getDocs,
-  collection,
-  deleteDoc,
-  updateDoc,
-  doc,
+    getFirestore,
+    getDocs,
+    collection,
+    deleteDoc,
+    updateDoc,
+    doc,
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
 export const AllContext = ({ children }) => {
-  const [usersList, setUserList] = useState([]);
-  const [productsList, setProductsList] = useState([]);
-  const [modificateProduct, setModificateProduct] = useState([]);
-  const navigate = useNavigate();
-  const db = getFirestore();
-  const querySnapshotUsuarios = collection(db, "Usuarios");
-  const querySnapshotProductos = collection(db, "Productos");
+    const [usersList, setUserList] = useState([]);
+    const [productsList, setProductsList] = useState([]);
+    const [modificateProduct, setModificateProduct] = useState([]);
+    const navigate = useNavigate();
+    const db = getFirestore();
+    const querySnapshotUsuarios = collection(db, "Usuarios");
+    const querySnapshotProductos = collection(db, "Productos");
 
-  const getUsuarios = () => {
-    //ACCEDER A LOS USER DE LA DB DE FIRESTORE
-    getDocs(querySnapshotUsuarios)
-      .then((response) => {
-        const listUsers = response.docs.map((doc) => {
-          return { id: doc.id, ...doc.data() };
+    const getUsuarios = () => {
+        //ACCEDER A LOS USER DE LA DB DE FIRESTORE
+        getDocs(querySnapshotUsuarios)
+            .then((response) => {
+                const listUsers = response.docs.map((doc) => {
+                    return { id: doc.id, ...doc.data() };
+                });
+                setUserList(listUsers);
+            })
+            .catch((error) => console.log(error));
+    };
+
+    const getProductos = () => {
+        getDocs(querySnapshotProductos)
+            .then((response) => {
+                const listProducts = response.docs.map((doc) => {
+                    return { id: doc.id, ...doc.data() };
+                });
+                listProducts.sort((x, y) => x.Nombre.localeCompare(y.Nombre));
+                setProductsList(listProducts);
+            })
+            .catch((error) => console.log(error));
+    };
+
+    useEffect(() => {
+        setTimeout(() => {
+            getUsuarios(); //HAGO UN TIMER PARA ALMACENAR LOS DATOS EN EL ARRAY LOCAL
+            getProductos();
+        }, "2000");
+    }, []);
+
+    const deleteUser = (docDelete) => {
+        deleteDoc(doc(db, "Usuarios", docDelete)); //ELIMINO EL USUARIO CON EL DOCUMENTO COMPLETO
+        Swal.fire({
+            icon: "success",
+            title: "VENDEDOR ELIMINADO CON EXITO",
+            timer: 2000,
         });
-        setUserList(listUsers);
-      })
-      .catch((error) => console.log(error));
-  };
+        navigate("/admin");
+    };
 
-  const getProductos = () => {
-    getDocs(querySnapshotProductos)
-      .then((response) => {
-        const listProducts = response.docs.map((doc) => {
-          return { id: doc.id, ...doc.data() };
+    const refreshPrice = (idProduct, newPrice) => {
+        updateDoc(doc(db, "Productos", idProduct), {
+            Precio: parseInt(newPrice),
         });
-        listProducts.sort((x, y) => x.Nombre.localeCompare(y.Nombre));
-        setProductsList(listProducts);
-      })
-      .catch((error) => console.log(error));
-  };
+        Swal.fire({
+            icon: "success",
+            title: "PRECIO MODIFICADO CON EXITO",
+            timer: 2000,
+        });
+        navigate("/admin");
+    };
 
-  useEffect(() => {
-    setTimeout(() => {
-      getUsuarios(); //HAGO UN TIMER PARA ALMACENAR LOS DATOS EN EL ARRAY LOCAL
-      getProductos();
-    }, "2000");
-  }, []);
+    const refreshStock = (idProduct, newStock) => {
+        updateDoc(doc(db, "Productos", idProduct), {
+            Stock: parseInt(newStock),
+        });
+        Swal.fire({
+            icon: "success",
+            title: "STOCK MODIFICADO CON EXITO",
+            timer: 2000,
+        });
 
-  const deleteUser = (docDelete) => {
-    deleteDoc(doc(db, "Usuarios", docDelete)); //ELIMINO EL USUARIO CON EL DOCUMENTO COMPLETO
-    Swal.fire({
-      icon: "success",
-      title: "VENDEDOR ELIMINADO CON EXITO",
-      timer: 2000,
-    });
-    getUsuarios(); //REFRESCO LA BD
-    navigate("/admin");
-  };
+        navigate("/admin");
+    };
 
-  const refreshPrice = (idProduct, newPrice) => {
-    updateDoc(doc(db, "Productos", idProduct), {
-      Precio: parseInt(newPrice),
-    });
-    Swal.fire({
-      icon: "success",
-      title: "PRECIO MODIFICADO CON EXITO",
-      timer: 2000,
-    });
-    setTimeout(() => {
-      getProductos();
-    }, 2000);
-    navigate("/stock");
-  };
+    const refreshName = (idProduct, newName) => {
+        updateDoc(doc(db, "Productos", idProduct), {
+            Nombre: newName,
+        });
+        Swal.fire({
+            icon: "success",
+            title: "NOMBRE MODIFICADO CON EXITO",
+            timer: 2000,
+        });
+        navigate("/admin");
+    };
 
-  const refreshStock = (idProduct, newStock) => {
-    updateDoc(doc(db, "Productos", idProduct), {
-      Stock: parseInt(newStock),
-    });
-    Swal.fire({
-      icon: "success",
-      title: "STOCK MODIFICADO CON EXITO",
-      timer: 2000,
-    });
-    setTimeout(() => {
-      getProductos();
-    }, 2000);
-    navigate("/stock");
-  };
+    const refreshSabor = (idProduct, newSabor) => {
+        updateDoc(doc(db, "Productos", idProduct), {
+            Sabor: newSabor,
+        });
+        Swal.fire({
+            icon: "success",
+            title: "SABOR MODIFICADO CON EXITO",
+            timer: 2000,
+        });
 
-  const refreshName = (idProduct, newName) => {
-    updateDoc(doc(db, "Productos", idProduct), {
-      Nombre: newName,
-    });
-    Swal.fire({
-      icon: "success",
-      title: "NOMBRE MODIFICADO CON EXITO",
-      timer: 2000,
-    });
-    setTimeout(() => {
-      getProductos();
-    }, 2000);
-    navigate("/stock");
-  };
+        navigate("/admin");
+    };
 
-  const refreshSabor = (idProduct, newSabor) => {
-    updateDoc(doc(db, "Productos", idProduct), {
-      Sabor: newSabor,
-    });
-    Swal.fire({
-      icon: "success",
-      title: "SABOR MODIFICADO CON EXITO",
-      timer: 2000,
-    });
+    const refreshCode = (idProduct, newCode) => {
+        updateDoc(doc(db, "Productos", idProduct), {
+            Codigo: parseInt(newCode),
+        });
+        Swal.fire({
+            icon: "success",
+            title: "CODIGO MODIFICADO CON EXITO",
+            timer: 2000,
+        });
+        navigate("/admin");
+    };
 
-    setTimeout(() => {
-      getProductos();
-    }, 2000);
+    const refreshImg = (idProduct, newImg) => {
+        updateDoc(doc(db, "Productos", idProduct), {
+            img: newImg,
+        });
+        Swal.fire({
+            icon: "success",
+            title: "IMÁGEN MODIFICADA CON EXITO",
+            timer: 2000,
+        });
 
-    navigate("/stock");
-  };
+        navigate("/admin");
+    };
 
-  const refreshCode = (idProduct, newCode) => {
-    updateDoc(doc(db, "Productos", idProduct), {
-      Codigo: parseInt(newCode),
-    });
-    Swal.fire({
-      icon: "success",
-      title: "CODIGO MODIFICADO CON EXITO",
-      timer: 2000,
-    });
-    setTimeout(() => {
-      getProductos();
-    }, 2000);
-    navigate("/stock");
-  };
+    const AddStock = (idProduct, newStock, currentStock) => {
+        const newStockModify = parseInt(currentStock) + parseInt(newStock);
+        updateDoc(doc(db, "Productos", idProduct), {
+            Stock: newStockModify,
+        });
+        Swal.fire({
+            icon: "success",
+            title: "STOCK MODIFICADO CON EXITO",
+            timer: 2000,
+        });
+        navigate("/admin");
+    };
 
-  const refreshImg = (idProduct, newImg) => {
-    updateDoc(doc(db, "Productos", idProduct), {
-      img: newImg,
-    });
-    Swal.fire({
-      icon: "success",
-      title: "IMÁGEN MODIFICADA CON EXITO",
-      timer: 2000,
-    });
-    setTimeout(() => {
-      getProductos();
-    }, 2000);
-    navigate("/stock");
-  };
+    const deleteProduct = (idProduct) => {
+        deleteDoc(doc(db, "Productos", idProduct)); //ELIMINO EL USUARIO CON EL DOCUMENTO COMPLETO
+        Swal.fire({
+            icon: "success",
+            title: "PRODUCTO ELIMINADO CON EXITO",
+            timer: 2000,
+        });
 
-  const AddStock = (idProduct, newStock, currentStock) => {
-    const newStockModify = parseInt(currentStock) + parseInt(newStock);
-    updateDoc(doc(db, "Productos", idProduct), {
-      Stock: newStockModify,
-    });
-    Swal.fire({
-      icon: "success",
-      title: "STOCK MODIFICADO CON EXITO",
-      timer: 2000,
-    });
-    setTimeout(() => {
-      getProductos();
-    }, 2000);
-    navigate("/admin");
-  };
+        navigate("/admin");
+    };
 
-  const deleteProduct = (idProduct) => {
-    deleteDoc(doc(db, "Productos", idProduct)); //ELIMINO EL USUARIO CON EL DOCUMENTO COMPLETO
-    Swal.fire({
-      icon: "success",
-      title: "PRODUCTO ELIMINADO CON EXITO",
-      timer: 2000,
-    });
-    setTimeout(() => {
-      getProductos();
-    }, 2000);
-    navigate("/stock");
-  };
-
-  return (
-    <BDContext.Provider
-      value={{
-        usersList,
-        getUsuarios,
-        deleteUser,
-        productsList,
-        setProductsList,
-        getProductos,
-        refreshPrice,
-        refreshStock,
-        refreshName,
-        refreshSabor,
-        refreshCode,
-        refreshImg,
-        modificateProduct,
-        setModificateProduct,
-        AddStock,
-        deleteProduct,
-      }}
-    >
-      {children}
-    </BDContext.Provider>
-  );
+    return (
+        <BDContext.Provider
+            value={{
+                usersList,
+                getUsuarios,
+                deleteUser,
+                productsList,
+                setProductsList,
+                getProductos,
+                refreshPrice,
+                refreshStock,
+                refreshName,
+                refreshSabor,
+                refreshCode,
+                refreshImg,
+                modificateProduct,
+                setModificateProduct,
+                AddStock,
+                deleteProduct,
+            }}
+        >
+            {children}
+        </BDContext.Provider>
+    );
 };
